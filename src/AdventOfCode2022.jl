165fca0e0088480bc2509928e9cab378d85b628d
Erik Steiger <rkstgr@gmail.com>
2022-12-01 12:33:51 +0100

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.29

---
+++ src/AdventOfCode2022.jl
@@ -0,0 +1,5 @@
+module AdventOfCode2022
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using AdventOfCode2022
+using Test
+
+@testset "AdventOfCode2022.jl" begin
+    # Write your tests here.
+end
