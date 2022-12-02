68544af63e86acd6393c9061167a084f1b12d3cb
Erik Steiger <rkstgr@gmail.com>
2022-12-02 13:40:50 +0100

    Tidy up solution of Day 02

--- src/day02.jl
+++ src/day02.jl
@@ -4,16 +4,6 @@ using AdventOfCode2022
 using Chain
 using Match
 
-# part2
-# 1 loose, 2 draw, 3 win
-# hand to play
-# 1 1 => 3
-# if its a 1:
-#   
-
-# 1 2 => 1
-# 1 3 => 2
-
 function day02(input::String = readInput(joinpath(@__DIR__, "..", "data", "day02.txt")))
 
     games = @chain input begin
@@ -22,9 +12,13 @@ function day02(input::String = readInput(joinpath(@__DIR__, "..", "data", "day02
         map(x -> split(x, " "), _)
         map(x -> (findfirst(only(x[1]), "ABC"), findfirst(only(x[2]), "XYZ")), _) # A X -> 1, 1
     end
+    
+    function result(x)
+        return x[2] + [3, 6, 0][(x[2]-x[1]+ 3) % 3 + 1]
+    end
 
     part1 = @chain games begin
-        map(x -> x[2] + [3, 6, 0][(x[2]-x[1]+ 3) % 3 + 1], _)
+        map(result, _)
         sum
     end
 
@@ -41,7 +35,7 @@ function day02(input::String = readInput(joinpath(@__DIR__, "..", "data", "day02
 
     part2 = @chain games begin
         map(x -> (x[1], hand2(x[1], x[2])), _)
-        map(x -> x[2] + [3, 6, 0][(x[2]-x[1]+ 3) % 3 + 1], _)
+        map(result, _)
         sum
     end
     
