a8f297d6933f55a9f00df2ea4091b62b2cf16d93
Erik Steiger <rkstgr@gmail.com>
2022-12-09 15:04:12 +0100

    Update Day 9: Preallocate result vectors

--- src/day09.jl
+++ src/day09.jl
@@ -26,16 +26,16 @@ function moveTail(tail::Vector{Int}, head::Vector{Int})::Vector{Int}
     if maximum(abs.(delta)) <= 1
         return tail
     end
-    delta = clamp.(delta, -1, 1)
-    return tail + delta
+    return tail + clamp.(delta, -1, 1)
 end
 
 function tailPositions(n, positions)::Vector{Vector{Int}}
-    if n == 1
-        return accumulate(moveTail, positions, dims=1)
-    else
-        return accumulate(moveTail, tailPositions(n-1, positions), dims=1)
+    result = Vector{Vector{Int}}(undef, size(positions))
+    for _ in 1:n
+        accumulate!(moveTail, result, positions)
+        positions = result
     end
+    return result
 end
 
 function day09(input = readInput(9))::Vector{Int}
